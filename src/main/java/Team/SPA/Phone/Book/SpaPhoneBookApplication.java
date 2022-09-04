f6d989d92d64e7333554fe606e0d13bc9bc3c6bc
dmitry <dmitry.mikhailov@devim.team>
2022-09-04 13:40:37 +0300

    add project SpringBoot

---
+++ src/main/java/Team/SPA/Phone/Book/SpaPhoneBookApplication.java
@@ -0,0 +1,13 @@
+package Team.SPA.Phone.Book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaPhoneBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaPhoneBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Team/SPA/Phone/Book/SpaPhoneBookApplicationTests.java
@@ -0,0 +1,13 @@
+package Team.SPA.Phone.Book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaPhoneBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
